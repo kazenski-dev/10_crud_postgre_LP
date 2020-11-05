9457679718bd0457a3867446a1de1bf1f7bac827
Kazenski-dev <56809101+kazenski-dev@users.noreply.github.com>
2020-11-05 16:04:13 -0300

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -2,5 +2,5 @@ import psycopg2
 
 class Conexao():
     def get_connection(self):
-        conexao = psycopg2.connect(user="postgres",password="8pcapdoe6gdd", host="127.0.0.1", port="5432", database="pessoa_lp")
+        conexao = psycopg2.connect(user="postgres",password="******", host="127.0.0.1", port="5432", database="pessoa_lp")
         return conexao
